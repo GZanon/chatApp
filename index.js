8a67fe9dd011b24f11c2ec0919ea6bbc0af1afd7
Giovane Zanon <giovanezanon@gmail.com>
2016-09-24 01:47:31 -0300

    index files

---
+++ index.js
@@ -0,0 +1,31 @@
+/* codigo de criacao do servidor sem socket.io
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+*/
+// codigo com socket.io
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+	console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
